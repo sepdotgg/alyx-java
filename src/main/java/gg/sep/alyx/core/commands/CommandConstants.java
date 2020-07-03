0335fc5f5cc46630d19e59231ae9b524dea90881
Seputaes <seputaes@sep.gg>
2020-07-03 12:15:25 -0700

    Add Alyx Plugin/Command constants file

---
+++ src/main/java/gg/sep/alyx/core/commands/CommandConstants.java
@@ -0,0 +1,17 @@
+package gg.sep.alyx.core.commands;
+
+/**
+ * Constants for Alyx command/plugin packages.
+ */
+public final class CommandConstants {
+    /**
+     * The serial number to uniquely identify Alyx plugins.
+     *
+     * This number is the Discord ID/snowflake of {@code Sep#0001}.
+     *
+     * See {@link AlyxPlugin#getIdentifier()}.
+     */
+    public static final long ALYX_PLUGIN_SERIAL = 239934148704337930L;
+
+    private CommandConstants() {}
+}
